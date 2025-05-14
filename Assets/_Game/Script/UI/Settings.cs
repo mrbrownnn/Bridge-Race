7e7fb90a03bc34c3c8ccd7c18651b8dd4d3cd804
mrbrownnn <thanhpham4655@gmail.com>
2025-05-14 15:31:15 +0700

    upload project

---
+++ Assets/_Game/Script/UI/Settings.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : UICanvas
+{
+    public override void Open()
+    {
+        Time.timeScale = 0;
+        base.Open();
+    }
+
+    public override void Close()
+    {
+        Time.timeScale = 1;
+        base.Close();
+    }
+
+    public void ContinueButton()
+    {
+        Close();
+    }
+
+    public void RetryButton()
+    {
+        LevelManager.Instance.OnRetry();
+        Close();
+    }
+}
